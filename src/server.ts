b078ea1e6c0a53443fc23cefc4894bc0657c2099
Bruno Carvalho <bru.ale.chimenes@gmail.com>
2023-09-17 18:50:55 -0300

    implemented and configured the fastify/cors library

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-videos';
 import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAICompletion } from './routes/generate-ia-completion';
 
 const app = fastify();
+
+app.register(fastifyCors, {
+    origin: '*'
+});
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAICompletion);
 
 app.listen({
     port: 3333,
